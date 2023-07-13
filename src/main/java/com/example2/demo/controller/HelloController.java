5fe6d8a834e17fa18e2b6ed1ef15965051dcdfaf
sojeong <tipani1623@naver.com>
2023-07-13 22:38:20 +0900

    main에서 추가

--- src/main/java/com/example2/demo/controller/HelloController.java
+++ src/main/java/com/example2/demo/controller/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "hello@@");
+        model.addAttribute("data", "main@@");
         return "hello";
     }
     
